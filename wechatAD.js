f827745be9018e87e986e12a5890bd4a2b346341
Sagittarius16th <73019664+Sagittarius16th@users.noreply.github.com>
2021-09-28 19:16:27 +0800

    Update wechatAD.js

--- wechatAD.js
+++ wechatAD.js
@@ -1,10 +1,8 @@
 /*
 微信公众号Ad
 QX:
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/lvyazi/quanx/main/Rewrite/WechatAd.js
-Surge4：
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/lvyazi/quanx/main/Rewrite/WechatAd.js
-Surge & QX MITM = mp.weixin.qq.com,
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/Sagittarius16th/quantumult-X/edit/main/WechatAd.js
+
 */
 
 var obj = JSON.parse($response.body);
